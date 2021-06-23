6f5913eeb58f1c1fe72d94dbbee41933588dce88
Sherpad Ndabambi <sgndabambi@gmail.com>
2021-06-23 16:30:06 +0200

    Modified pomodoro page

--- pomodoro.php
+++ pomodoro.php
@@ -43,7 +43,7 @@ function logout(){
 
 	<button id="accountButton" onclick="show(accountPanel)" class="right"> <img id="accountIcon" src="files/images/icons8-user-24.png"> </button>
 
-	<div id="accountPanel" class="hidden; right">
+	<div id="accountPanel" class="hidden right">
 		
 		<button id="accountButton" onclick="hide(accountPanel)"> <img id="accountIcon" src="files/images/icons8-user-24.png"> </button>
 
@@ -65,25 +65,26 @@ function logout(){
 	
 		<form id="sessionForm" action="files/logSession.php" method="post">
 
-			<input id="task_name" name="task_name" onblur="this.placeholder = 'Unnamed Task'" onfocus="this.placeholder = ''" placeholder="Unnamed Task">
+			<input id="taskName" name="task_name" onblur="this.placeholder = 'Unnamed Task'" onfocus="this.placeholder = ''" placeholder="Unnamed Task">
 
-			<input id="start_date" name="start_date" class="hidden">
+			<input id="startDate" name="start_date" class="hidden">
 		
-			<input id="start_time" name="start_time" class="hidden">
-			<input id="stop_time" name="stop_time" class="hidden">
-			<input id="time_elapsed" name="time_elapsed" class="hidden">
+			<input id="startTime" name="start_time" class="hidden">
+			<input id="stopTime" name="stop_time" class="hidden">
+			<input id="timeElapsed" name="time_elapsed" class="hidden">
 
 		</form>
 
-		<button id="playButton" onclick="play()"> <img id="playIcon" src="files/images/play.png" class="control"> </button>
-		<button id="pauseButton" onclick="pause()"> <img id="pauseIcon" src="files/images/pause.png" class="control"> </button>
-		<button id="stopButton" onclick="stop()"> <img id="stopIcon" src="files/images/stop.png" class="control"> </button>
+		<button id="startButton" onclick="start()"> <img id="startIcon" src="files/images/play.png" class="control"> </button>
+		<button id="pauseButton" class="hidden"  onclick="pause()"> <img id="pauseIcon" src="files/images/pause.png" class="control"> </button>
+		<button id="stopButton" class="hidden" onclick="stop()"> <img id="stopIcon" src="files/images/stop.png" class="control"> </button>
 
 	</div>
 
-	<script type="text/javascript" src="files/scripts/time.js"></script>
-	<script type="text/javascript" src="files/scripts/scripts.js"></script>
-	<script type="text/javascript" src="files/scripts/pomodoro.js"></script>
+	<script type="text/javascript" src="files/js/scripts.js"></script>
+	<script type="text/javascript" src="files/js/time.js"></script>
+	<script type="text/javascript" src="files/js/timer.js"></script>
+	<script type="text/javascript" src="files/js/pomodoro.js"></script>
 
 </body>
 
