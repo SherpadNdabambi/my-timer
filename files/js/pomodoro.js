b9d23963ce8fa61c626da79cac4a77937ef794d1
Sherpad Ndabambi <sgndabambi@gmail.com>
2021-06-27 16:02:17 +0200

    corrected calculations

--- files/js/pomodoro.js
+++ files/js/pomodoro.js
@@ -19,9 +19,9 @@ function addContextMenu() {
 
 function calculateTimeWorked(){
 	timeWorked = new Time();
-	timeWorked.hours = workIteration - 1 x workTime.hours;
-	timeWorked.minutes = workIteration - 1 x workTime.minutes;
-	timeWorked.seconds = workIteration - 1 x workTime.seconds;
+	timeWorked.hours = (workIteration - 1) * workTime.hours;
+	timeWorked.minutes = (workIteration - 1) * workTime.minutes;
+	timeWorked.seconds = (workIteration - 1) * workTime.seconds;
 	if(/Work/.test(phase)) if(phase = "Extra Work Time") timeWorked = timeWorked.plus(workTime);
 	else timeWorked = timeWorked.plus(workTime.minus(timeLeft));
 }
