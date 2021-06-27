caa3bfd371f1c11fdffafa7135afbacb1c42ec03
Sherpad Ndabambi <sgndabambi@gmail.com>
2021-06-27 15:57:19 +0200

    Implemented logPomodoroSession script

--- files/php/logPomodoroSession.php
+++ files/php/logPomodoroSession.php
@@ -1,5 +1,6 @@
 <?php
 
+//create connection to database
 $sqlConnection = new mysqli("localhost", "root", "root", "my_timer") or die("Connection failed: " .$sqlConnection->connect_error);
 
 //get session data from session form
@@ -17,5 +18,16 @@ $date_stopped = stripslashes($date_stopped);
 $time_started = stripslashes($time_started);
 $time_stopped = stripslashes($time_stopped);
 $time_worked = stripslashes($time_worked);
+
+//create task
+$createTaskQuery = "insert ignore into task (user_id, name) values ($_SESSION['userID'], '$task_name');";
+$sqlConnection->query($createTaskQuery);
+
+//create session
+$createSessionQuery = "insert into session (task_id, user_id, date_started, date_stopped, time_started, time_stopped, time_worked, type) values ((select id from task where name = '$task_name'), '$_SESSION['userID']', '$date_started', '$date_stopped', '$time_started', '$time_stopped', 'pomodoro');";
+$sqlConnection->query($createSessionQuery);
+
+//close connection to database
+$sqlConnection->close();
 
 ?>
